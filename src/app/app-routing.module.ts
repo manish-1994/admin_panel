b9b6fb2d3c8f0b4e498fb6188d9190983aceb2ac
manny-1994 <paramguru45@gmail.com>
2019-10-27 18:13:01 +0530

    the validation part without editing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UploadNoticeComponent} from './upload-notice/upload-notice.component';
+import {EnquiryComponent} from './enquiry/enquiry.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'uploadNotice', component: UploadNoticeComponent},
+  {path: 'Enquiry', component: EnquiryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,64 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadNoticeComponent } from './upload-notice/upload-notice.component';
+import { EnquiryComponent } from './enquiry/enquiry.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {
+  MatCardModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatOptionModule, MatProgressBarModule, MatSelectModule, MatSortModule, MatTableModule
+} from '@angular/material';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {FormsModule} from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadNoticeComponent,
+    EnquiryComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    FormsModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatSortModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
